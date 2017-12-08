dfac4a7120f36d8576786047c10ee54b37e878a8
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-08 17:46:44 -0600

    Add files via upload

---
+++ Date.js
@@ -0,0 +1,16 @@
+//  Created by Shawn O'Grady on 12/8/17.
+//  Copyright © 2017 Shawn O'Grady. All rights reserved.
+//
+/*
+ This code is a practice JavaScript interview question from testdome.com
+https://www.testdome.com/questions/javascript/date/8521?visibility=1&skillId=2
+
+ Problem statement: Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD)
+    -The parameter "userDate" and the return value are strings.
+    -For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+ */
+function formatDate(userDate) {
+  // format from M/D/YYYY to YYYYMMDD
+}
+
+console.log(formatDate("12/31/2014"));
