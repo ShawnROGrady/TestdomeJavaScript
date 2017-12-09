a634fdc5d15d1ede13bb6d9db06e365ad566c7f1
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-08 18:12:12 -0600

    Implemented formatDate() function
    Implemented full functionality of the formatDate() function. Passes 4/4 tests on testdome.com

--- Date.js
+++ Date.js
@@ -8,9 +8,25 @@ https://www.testdome.com/questions/javascript/date/8521?visibility=1&skillId=2
  Problem statement: Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD)
     -The parameter "userDate" and the return value are strings.
     -For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+
+ + Passes 4/4 tests
  */
 function formatDate(userDate) {
-  // format from M/D/YYYY to YYYYMMDD
+  var friendlyDate; //Date in format require by API
+  var splitDate=userDate.split('/');
+  if(splitDate[0].length==1){
+    //one digit month
+    splitDate[0]='0'+splitDate[0];
+  }
+  if(splitDate[1].length==1){
+    //one digit date
+    splitDate[1]='0'+splitDate[1];
+  }
+  friendlyDate=splitDate[2]+splitDate[0]+splitDate[1];
+  return friendlyDate;
 }
 
-console.log(formatDate("12/31/2014"));
+console.log(formatDate("12/31/2014"));  //20141231
+console.log(formatDate("1/31/2014"));  //20140131
+console.log(formatDate("12/3/2014"));  //20141203
+console.log(formatDate("11/3/2014"));  //20140103
